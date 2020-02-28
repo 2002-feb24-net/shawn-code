497572c22f8c1b693e184261d5f516c9a98ca106
shawnhalcomb <shawnhalcomb@gmail.com>
2020-02-28 10:17:43 -0600

    Completed Acronym and moved in SubString project.

---
+++ SubString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace TEST123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Provide a string and a number: ");
+            string UserInput = Console.ReadLine();
+            if (UserInput.Contains(' ') == false)
+            {
+                Console.WriteLine("Invalid response, please try again.");
+                Environment.Exit(0);
+            }
+            string[] VerifiedInput = UserInput.Split(' ');
+            string str = VerifiedInput[0];
+            int num = int.Parse(VerifiedInput[1]);
+            for (int i = 1; i < str.Length; i++)
+            {
+                for (int start = 0; start <= str.Length - i; start++)
+                {
+                    string substr = str.Substring(start, i);
+                    if (substr.Length == num)
+                    {
+                        Console.WriteLine(substr);
+                    }
+                }
+            }
+        }
+    }
+}
