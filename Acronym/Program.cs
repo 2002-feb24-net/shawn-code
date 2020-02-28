39cdffdb06afc5fea6f3bb61bde5653c928a45ea
shawnhalcomb <shawnhalcomb@gmail.com>
2020-02-28 09:37:26 -0600

    Completed Acronym Program

--- Acronym/Program.cs
+++ Acronym/Program.cs
@@ -6,7 +6,20 @@ namespace Acronym
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.Write("Please provide more than 1 word: ");
+            String UserInput = Console.ReadLine();
+            string Acronym = Abbreviate(UserInput);
+            System.Console.WriteLine(Acronym);
+        }
+        public static string Abbreviate(string UserInput)
+        {
+            string[] FullString = UserInput.Split(new char[] { ' ', '_', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            string UpperCaseLetters = "";
+            foreach (string word in FullString)
+            {
+                UpperCaseLetters += word[0].ToString().ToUpper();
+            }
+            return UpperCaseLetters;
         }
     }
 }
