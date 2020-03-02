a4b11ee7cc07205b6ae463d3344a94b6eaf1e09c
shawnhalcomb <shawnhalcomb@gmail.com>
2020-03-02 13:47:49 -0600

    FizzBuzz Project added

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumFizz = 0;
+            int NumBuzz = 0;
+            int NumFizzBuzz = 0;
+
+            System.Console.WriteLine();
+            for (int i = 0; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    System.Console.WriteLine(i + " Fizz");
+                    NumFizz++;
+                }
+                if (i % 3 != 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine(i + " Buzz");
+                    NumBuzz++;
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine(i + " FizzBuzz");
+                    NumFizzBuzz++;
+                }
+                else if (i % 3 != 0 && i % 5 != 0)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+            System.Console.Write("Total Fizz = " + NumFizz + ' ');
+            System.Console.WriteLine("Total Buzz = " + NumBuzz);
+            System.Console.WriteLine("Total FizzBuzz = " + NumFizzBuzz);
+            /* 
+            FIZZ = 27 
+            BUZZ = 14
+            FIZZBUZZ = 7
+            */
+        }
+    }
+}
---
+++ GetFromArray/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] words = {"One", "Two", "Three"};
+            System.Console.WriteLine(words[2].Substring(0,1));
+        }
+    }
+}
